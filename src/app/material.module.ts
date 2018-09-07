e31922f2764be9a27984aa2ab67587b9ff372149
unknown <m.xiyau@gmail.com>
2018-09-08 02:54:45 +0500

    added a datepicker to get birthday and set max to 18 years

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from "@angular/core";
-import { MatButtonModule, MatIconModule,MatFormFieldModule,MatInputModule, MatInput} from '@angular/material';
+import { MatButtonModule, MatIconModule,MatFormFieldModule,MatInputModule, MatDatepickerModule,MatNativeDateModule} from '@angular/material';
 
 
 @NgModule({
-    imports: [MatButtonModule,MatIconModule,MatFormFieldModule,MatInputModule],
-    exports: [MatButtonModule,MatIconModule,MatFormFieldModule,MatInputModule]
+    imports: [MatButtonModule,MatIconModule,MatFormFieldModule,MatInputModule,MatDatepickerModule,MatNativeDateModule],
+    exports: [MatButtonModule,MatIconModule,MatFormFieldModule,MatInputModule,MatDatepickerModule,MatNativeDateModule]
 })
 export class MaterialModule {}
