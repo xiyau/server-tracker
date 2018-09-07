856d4d73c64fc9cc5ddbe2d5f256515b91f8f533
unknown <m.xiyau@gmail.com>
2018-09-08 02:07:21 +0500

    installed flex-layout and added to imports and set fx-layout to column

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
+import {FlexLayoutModule} from '@angular/flex-layout';
 import { AppComponent } from './app.component';
 import { MaterialModule} from './material.module';
 import { SignupComponent } from './auth/signup/signup.component';
@@ -22,7 +22,8 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
